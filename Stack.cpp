b100dc1e57a92bce74afcf1e51d83acfd7461527
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-01-14 19:18:04 +0600

    stack functions and its uses

---
+++ Stack.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    // its a last in first out(LIFO) system;
+    stack<ll> s; // declaring
+    // access element
+    s.top();
+    // modification
+    s.push(1); // insert
+    s.pop();   // delete
+    s.size();  // size calculation
+}
