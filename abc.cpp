0aa9f9979ac16789c0ae77962f1c3cd5beabd406
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-01-17 22:10:34 +0600

    code template

--- abc.cpp
+++ abc.cpp
@@ -7,6 +7,21 @@ int main()
     cin >> t;
     while (t--)
     {
-        
+        ll n, m;
+        cin >> n >> m;
+        ll p = 0;
+        for (ll i = 0; i < n; i++)
+        {
+            ll x, y;
+            cin >> x >> y;
+            if (i > 0)
+            {
+                ll x = ((m - x) + (m - y)) * 2;
+                p += x;
+            }
+        }
+        ll q = (4 * m) * n;
+        ll ans = p - p;
+        cout << ans << endl;
     }
 }
